d5beee78a42c78d73783bf8af0144f9693fe5e11
Hakim Waithaka <hakim.waithaka@gmail.com>
2025-02-18 13:32:56 +0300

    commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()  # Normalize to lowercase
+
+    def match(self, word_list):
+        sorted_word = sorted(self.word)  # Sort letters of base word
+        return [word for word in word_list if sorted(word.lower()) == sorted_word]
+
